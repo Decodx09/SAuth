0aad603e37219ff25483163965990cf1b29cf14b
shivansh <53109024+Decodx09@users.noreply.github.com>
2025-06-11 03:48:32 +0530

    jenkins-file

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [{
+      name: 'node-app',
+      script: 'src/index.js',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9000
+      }
+    }]
+  };
